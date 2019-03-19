30e577a309b3a282f1fba45cd1794e08b62d9581
mscharstrom <mikael@scharstrom.com>
2019-03-19 13:22:50 +0100

    updated structure

---
+++ dota_snitch/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+""" Enter your own personal data.
+These variables are imported to
+dota_snitch.py and are required. """
+
+STEAMAPI_KEY = ""  # Add your Steam API key
+MY_ID = ""  # Add you Steam ID (in numbers).
+HUE_ID = ""  # Add your Hue username
+HUE_IP = ""  # Your Hue bridge IP
