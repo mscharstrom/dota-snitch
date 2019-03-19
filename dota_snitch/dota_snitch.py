8cfb18a7fbde306e939a239b710d30877066bc11
mscharstrom <mikael@scharstrom.com>
2019-03-19 13:21:35 +0100

    updated structure and file import

--- dota_snitch/dota_snitch.py
+++ dota_snitch/dota_snitch.py
@@ -6,31 +6,7 @@ friends are in a specific game. """
 
 import json
 import requests
-
-STEAMAPI_KEY = ""  # Add your Steam API key
-MY_ID = ""  # Add you Steam ID (in numbers).
-
-
-def main():
-
-    """ Test function to see my own status.
-    This will be removed on first realease """
-
-    req = requests.get(f'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={STEAMAPI_KEY}&steamids={MY_ID}')
-    json_response = json.loads(req.content)
-
-    personaname = json_response["response"]["players"][0]["personaname"]
-    personastate = json_response["response"]["players"][0]["personastate"]
-
-    print()
-    print("name: " + personaname)
-    print("state (online/offline etc): " + str(personastate))
-
-    if "gameid" not in json_response["response"]["players"][0]:
-        print("In game: None")
-    else:
-        persona_gameid = json_response["response"]["players"][0]["gameid"]
-        print("In game: " + persona_gameid)
+import setup
 
 
 def get_friends():
@@ -39,34 +15,33 @@ def get_friends():
     From this data we will be starting
     the LED-Strip and make it light up """
 
-    req = requests.get(f'http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={STEAMAPI_KEY}&steamid={MY_ID}&relationship=friend')
+    req = requests.get(f'http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={setup.STEAMAPI_KEY}&steamid={setup.MY_ID}&relationship=friend')
     friends_response = json.loads(req.content)
 
-    print()
-    print("Friends by SteamID:")
-
-    # Save friends steam ID to an set array.
+    # create  arrays for storing friends data
     friend_arr = set()
+    dota_arr = set()
+    online_arr = set()
+
     for friends in friends_response["friendslist"]["friends"]:
         friend_arr.add(friends["steamid"])
 
-    # Loop through friends and check their status
+    # loop through friends and check their status, save to arrays.
     for friends in friend_arr:
-        req_check = requests.get(f'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={STEAMAPI_KEY}&steamids={friends}')
+        req_check = requests.get(f'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={setup.STEAMAPI_KEY}&steamids={friends}')
         friends_check = json.loads(req_check.content)
 
         if "gameid" in friends_check["response"]["players"][0]:
             if friends_check["response"]["players"][0]["gameid"] == "570":
-                print(friends_check["response"]["players"][0]["personaname"] +
-                      " - " + friends_check["response"]
-                      ["players"][0]["gameid"])
+                dota_arr.add(friends_check["response"]["players"][0]["personaname"])
             elif friends_check["response"]["players"][0]["gameid"] != "570":
-                print(friends_check["response"]["players"][0]["personaname"] +
-                      " - " + "Playing other game")
+                online_arr.add(friends_check["response"]["players"][0]["personaname"])
         elif friends_check["response"]["players"][0]["personastate"] == 1:
-            print(friends_check["response"]["players"][0]["personaname"] +
-                  " - Online")
+            online_arr.add(friends_check["response"]["players"][0]["personaname"])
+
+    print(len(dota_arr))
+    print(online_arr)
+
 
 if __name__ == '__main__':
-    main()
     get_friends()
