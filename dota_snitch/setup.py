759bb323ce530f538fa0961c928c0c72c5c7cf6e
mscharstrom <mikael@scharstrom.com>
2019-03-27 19:18:17 +0100

    added hue_light

--- dota_snitch/setup.py
+++ dota_snitch/setup.py
@@ -8,3 +8,4 @@ STEAMAPI_KEY = ""  # Add your Steam API key
 MY_ID = ""  # Add you Steam ID (in numbers).
 HUE_ID = ""  # Add your Hue username
 HUE_IP = ""  # Your Hue bridge IP
+HUE_LIGHT = ""  # Your Hue light ID number
