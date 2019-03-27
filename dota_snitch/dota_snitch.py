f19816cc4a343f5630cc31ec3494166810baa3d0
mscharstrom <mikael@scharstrom.com>
2019-03-27 19:09:34 +0100

    pep8 fixes

--- dota_snitch/dota_snitch.py
+++ dota_snitch/dota_snitch.py
@@ -49,7 +49,7 @@ def get_friends():
         get_specific_online()
 
 def get_specific_online():
-    
+
     """If you are online and
     in dota, this function will
     light up the Hue. If not it will
